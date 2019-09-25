60d28757239f6ef5d16c9d4336246fbf1f6720af
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-09-25 18:11:29 +0300

    Made some refactoring

--- practice_2/task/auth.php
+++ practice_2/task/auth.php
@@ -6,7 +6,7 @@ $password = '1111';
 $loginPage = 'login.php';
 $cabinetPage = 'cabinet.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['password'] == $password && $_POST['login'] == $login) {
+if ($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['password'] == $password && $_POST['login'] == $login) {
     $_SESSION['auth'] = true;
     echo 'hello!';
     header('Location: '.$cabinetPage);

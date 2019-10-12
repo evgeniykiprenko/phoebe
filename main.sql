63d89013307da53b326fdafb3007f4fdd847dddb
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-10-13 02:21:49 +0300

    Made a workpiece for lab work

---
+++ main.sql
@@ -0,0 +1,20 @@
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255)
+);
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  photo VARCHAR(255),
+  role_id INT NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+INSERT INTO roles (title) VALUES ('admin');
+INSERT INTO roles (title) VALUES ('user');
+
+INSERT INTO users (first_name, last_name, password, role_id)
+VALUES ('admin', 'admin', 'admin', 1);

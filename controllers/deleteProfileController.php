1bab54ee43f858fc717a742ac0645223a67858d2
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-10-15 13:42:20 +0300

    Small fixed and made deleting of profile.

--- controllers/deleteProfileController.php
+++ controllers/deleteProfileController.php
@@ -1,1 +1,18 @@
 <?php
+include "dbUtils.php";
+
+session_start();
+
+
+$mainPage = '../public/index.php';
+$sql = 'DELETE FROM users WHERE id = ' . $_GET['id'] . ';';
+
+if ($_SESSION['id'] == $id) {
+    runQuery($sql);
+    session_unset();
+    session_destroy();
+} elseif ($_SESSION['role'] == 'admin') {
+    runQuery($sql);
+}
+header('Location: ' . $mainPage);
+
--- controllers/uploadController.php
+++ controllers/uploadController.php
@@ -2,12 +2,12 @@
 include "dbUtils.php";
 
 $id = $_GET['id'];
-
 $target_dir = "../public/images/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $file_name = basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+$sql = "UPDATE users SET photo = '$file_name' WHERE id = $id;";
 // Check if image file is a actual image or fake image
 if (isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -21,7 +21,8 @@ if (isset($_POST["submit"])) {
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    echo "File already exists. Using existing file";
+    runQuery($sql);
     $uploadOk = 0;
 }
 // Check file size
@@ -42,7 +43,6 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-        $sql = "UPDATE users SET photo = '$file_name' WHERE id = $id;";
         runQuery($sql);
     } else {
         echo "Sorry, there was an error uploading your file.";

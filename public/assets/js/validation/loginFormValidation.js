8e766a5938354d81ded9e287f5fd17178db46860
Eugene Kiprenko <zhenyakiprenko@gmail.com>
2019-12-05 16:59:35 +0200

    Making login form validation

--- public/assets/js/validation/loginFormValidation.js
+++ public/assets/js/validation/loginFormValidation.js
@@ -1,14 +1,21 @@
 function validateLogin() {
-    //Very pretty red color #C70039 
     let loginField = document.getElementById('login');
     let login = loginField.value;
-    let hint = document.getElementById('login_hint')
+    let hint = document.getElementById('login_hint');
+
+    let allowedSymbols = '^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    let fieldBorderColor = '#C70039';
+
+    loginField.style.borderColor = '';
+    hint.innerText = '';
     
     if (!login) {
-        hint.style.display = 'block';
         hint.innerText = 'The field is requried.';
-        hint.style.borderColor = '#C70039';
-    }
+        loginField.style.borderColor = fieldBorderColor;
+    } else if (!login.match(allowedSymbols)) {
+        hint.innerText = 'Please provide a valid email address.';
+        loginField.style.borderColor = fieldBorderColor;
+    } 
 }
 
 function valdiatePassword() {
--- public/index.php
+++ public/index.php
@@ -11,11 +11,12 @@ session_start();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/hint.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src ="/phoebe/public/assets/js/ajax/showUsersList.js"></script>
+    <script src="/phoebe/public/assets/js/ajax/showUsersList.js"></script>
     <script src="/phoebe/public/assets/js/validation/loginFormValidation.js"></script>
     <title>Phoebe</title>
 </head>
@@ -75,28 +76,28 @@ session_start();
 
                     <!-- Modal body -->
                     <div class="modal-body">
-                        <form action="/phoebe/controllers/authController.php" method="post">
-                            <div class="form-group">
+                        <!-- <form action="/phoebe/controllers/authController.php" method="post"> -->
+                        <form>
+                            <div>
                                 <label for="email">Email address:</label>
-                                <input type="text" id="login" name="email" required>
-                                <div id="login_hint"></div>
+                                <input type="text" id="login" name="email">
                             </div>
-                            </div>
-                            <div class="form-group">
+                            <div id="login_hint" class="hint"></div>
+                            <div>
                                 <label for="pwd">Password:</label>
-                                <input type="password" id="pwd" name="password" required>
+                                <input type="password" id="pwd" name="password">
                                 <div id="login_hint"></div>
-                            <button type="submit" class="btn btn-primary" onclick="validateLogin()">Submit</button>
+                                <button type="button" class="btn btn-primary" onclick="validateLogin()">Submit</button>
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="container">
-        <div id="users"></div>
-    </div>
+        <div class="container">
+            <div id="users"></div>
+        </div>
 </body>
 
 </html>
